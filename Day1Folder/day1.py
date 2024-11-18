196ca610c8f731f834007e4933d64de4a5fe63c7
jaredpoon <poonjunkaijared@gmail.com>
2024-11-18 04:35:36 +0000

    Day 1

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,93 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# Demostrating code sequence.
+
+# print("hello from day1")
+# print("I am Jared :)")
+# print("I am 10 years old.")
+# print("I attend Kuo Chuan Presbyterian School.")
+# print("If I had $2000, I would buy is the Oris Divers Sixty-Five 40mm Watch.")
+
+# firstName = "Jared"
+# lastName = "Poon"
+# numberOfAppleInTheBasket = "IDK"
+# print(firstName + " " + lastName)
+# print(numberOfAppleInTheBasket)
+
+# num1 = 10
+# num2 = 15
+# print(num1 + num2)
+
+# num1NotAdd = "10"
+# num2NotAdd = "15"
+# print(num1NotAdd + num2NotAdd)
+
+# num1IntegerAndString = "10"
+# num2IntegerAndString = 15
+# print(int(num1IntegerAndString) + num2IntegerAndString)
+
+# num1StringAndInteger = "10"
+# num2StringAndInteger = 15
+# print(num1StringAndInteger + str(num2StringAndInteger))
+
+# ans1 = 1+1
+# ans2 = 1-1
+# ans3 = 1*1
+# ans4 = 10 / 2
+# print(ans1)
+# print(ans2)
+# print(ans3)
+# print(ans4)
+# print(ans1 + ans2 + ans3 + ans4)
+
+# name = "Dog"
+# food = "Bone"
+# print("My name is " + name)
+# print("My favourite food is " + food)
+# print(name + " " + "says" + " " + food)
+
+
+# FirstName1 = "Jared"
+# LastName1 = "Poon"
+# print("My name is " + FirstName1)
+# print("My last name is " + LastName1)
+# print("My full name is " + FirstName1 + " " + LastName1)
+# print("My friends call me " + FirstName1)
+
+# FirstName2 = "Jared"
+# LastName2 = "Poon"
+# def introduceMyself():
+#     print("My name is " + FirstName2)
+#     print("My last name is " + LastName2)
+#     print("My full name is " + FirstName2 + " " + LastName2)
+#     print("My friends call me " + FirstName2)
+# introduceMyself()
+
+
+# def introduceMyself(FirstName3, LastName3):
+#     print("My name is " + FirstName3)
+#     print("My last name is " + LastName3)
+#     print("My full name is " + FirstName3 + " " + LastName3)
+#     print("My friends call me " + FirstName3)
+# introduceMyself("Jared", "Poon")
+# introduceMyself("Jathan", "Eagle")
+# introduceMyself("Alfred", "Chan")
+# introduceMyself("Thomas", "Lim")
+
+
+# def addition1(num01, num02):
+#     print(num01 + num02)
+# addition1(40000, 40000)
+# addition1(2, 3)
+# addition1(243, 236)
+# addition1(5000, 3)
+
+
+# def addition2(num03, num04):
+#     ans0102 = num03 + num04
+#     print(ans0102)
+# addition2(40000, 40000)
+# addition2(2, 3)
+# addition2(243, 236)
+# addition2(5000, 3)
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,33 @@
 
 
 ################## On Chat Commands Section #####################
+def Teleport():
+    agent.teleport_to_player()
+def moveForward():
+    agent.move(FORWARD, 1)
+def moveBackward():
+    agent.move(BACK, 1)
+def moveUp():
+    agent.move(UP, 1)
+def moveDown():
+    agent.move(DOWN, 1)
+def turnLeft():
+    agent.turn(TurnDirection.LEFT)
+def turnRight():
+    agent.turn(TurnDirection.RIGHT)
+def moveRight():
+    agent.move(RIGHT, 1)
+def moveLeft():
+    agent.move(LEFT,1)
+
+player.on_chat("fw", moveForward)
+player.on_chat("bk", moveBackward)
+player.on_chat("tl", turnLeft)
+player.on_chat("tr", turnRight)
+player.on_chat("up", moveUp)
+player.on_chat("down", moveDown)
+player.on_chat("mr", moveRight)
+player.on_chat("ml", moveLeft)
+player.on_chat("come", Teleport)
+
+mobs.kill(mobs.target(NEAREST_PLAYER))
