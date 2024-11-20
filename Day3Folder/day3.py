af5529a49769e19c0ff351ab72b2e98733ef1881
jaredpoon <poonjunkaijared@gmail.com>
2024-11-20 04:30:15 +0000

    Day 3

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,4 +1,105 @@
 # Write all your codes for Day 3 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day3")
+# print("hello from day3")
+
+
+# import time
+# name1 = input("What is your name?" + " ")
+# for i in name1:
+#     time.sleep(1)
+#     print("Give me a " + i + "!")
+# time.sleep(1)
+# print("WHO'S THE BEST?")
+# time.sleep(1)
+# print("IT'S")
+# for i in name1:
+#     time.sleep(1)
+#     print(i + "!")
+# print(name1 + "!!!!")
+
+
+# num1 = input("What is the first number to add? ")
+# num2 = input("What is the second number to add? ")
+# ans = int(num1) + int(num2)
+# for i in ans():
+#     print("The sum of " + num1 + " and " + num2 + " = " + str(ans))
+
+
+# name2 = input("What is your name? ")
+# num_pens = input("How many pens did you buy? ")
+# print(name2 + " bought " + num_pens + " pens.")
+
+
+# num1calc = input("What is the first number? ")
+# num2calc = input("What is the second number? ")
+# ans1calc = (int(num1calc) + int(num2calc))
+# ans2calc = (int(num1calc) - int(num2calc))
+# ans3calc = (int(num1calc) * int (num2calc))
+# ans4calc = (int(num1calc) / int(num2calc))
+# print(num1calc + " + " + num2calc + " = " + str(ans1calc))
+# print(num1calc + " - " + num2calc + " = " + str(ans2calc))
+# print(num1calc + " x " + num2calc + " = " + str(ans3calc))
+# print(num1calc + " ÷ " + num2calc + " = " + str(ans4calc))
+
+
+# num1calcteacher = int(input("What is the first number? "))
+# num2calcteacher = int(input("What is the second number? "))
+# print(str(num1calcteacher) + " + " + str(num2calcteacher) + " = " + str(num1calcteacher + num2calcteacher))
+# print(str(num1calcteacher) + " - " + str(num2calcteacher) + " = " + str(num1calcteacher - num2calcteacher))
+# print(str(num1calcteacher) + " x " + str(num2calcteacher) + " = " + str(num1calcteacher * num2calcteacher))
+# print(str(num1calcteacher) + " ÷ " + str(num2calcteacher) + " = " + str(num1calcteacher / num2calcteacher))
+
+
+# print(10 < 5)
+# print(10 > 5)
+# print(5 <= 10)
+# print(5 <= 5)
+# print(5 < 5)
+# print(5 == 5)
+# print(5 != 5)
+
+
+# correctPassword = "ABCD"
+# userPassword = input("What is the password? ")
+# print(correctPassword == userPassword)
+
+
+# correctPassword = "ABCD"
+# userPassword = input("What is the password? ")
+# if correctPassword == userPassword:
+#     print("Access Granted!")
+# if correctPassword != userPassword:
+#     print("Access Denied!")
+
+
+# correctPassword = "ABCD"
+# userPassword = input("What is the password? ")
+# if correctPassword == userPassword:
+#     print("Access Granted!")
+# else: 
+#     print("Access Denied!")
+
+
+# person1age = input("What is your age? ")
+# person2age = input("What is youur age? ")
+# if person1age > person2age:
+#     print("Person 1,lease take care of Person 2.")
+# elif person1age == person2age:
+#     print("Take care of each other and hold hands.")
+# else:
+#     print("Person 2, please take care of Person 1.")
+
+# import random
+# print(random.randint(1, 100))
+
+
+import random
+randomNum = random.randint(1, 5)
+userGuess = int(input("Guess a number between 1 and 5: "))
+if userGuess == randomNum:
+    print("You are correct!!!! You won!!!!")
+elif userGuess > randomNum:
+    print("You number is too large, try again!")
+elif userGuess < randomNum:
+    print("Your number is too small, try again!")
--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,63 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def Teleport():
+    agent.teleport_to_player()
+def moveForward(steps):
+    agent.move(FORWARD, steps)
+def moveBackward(steps):
+    agent.move(BACK, steps)
+def moveUp(steps):
+    agent.move(UP, steps)
+def moveDown(steps):
+    agent.move(DOWN, steps)
+def turnLeft():
+    agent.turn(TurnDirection.LEFT)
+def turnRight():
+    agent.turn(TurnDirection.RIGHT)
+def moveRight(steps):
+    agent.move(RIGHT, steps)
+def moveLeft(steps):
+    agent.move(LEFT, steps)
+
+player.on_chat("fw", moveForward)
+player.on_chat("bk", moveBackward)
+player.on_chat("tl", turnLeft)
+player.on_chat("tr", turnRight)
+player.on_chat("up", moveUp)
+player.on_chat("down", moveDown)
+player.on_chat("mr", moveRight)
+player.on_chat("ml", moveLeft)
+player.on_chat("come", Teleport)
+
+def kill():
+    mobs.kill(mobs.target(NEAREST_PLAYER))
+player.on_chat("a", kill)
+
+
+def destroyTree(steps):
+    for i in range(steps):
+        agent.destroy(FORWARD)
+        agent.move(UP, 1)
+    agent.move(DOWN, steps)
+    agent.collect_all()
+player.on_chat("dup", destroyTree)
+
+
+def mine(steps):
+    for i in range(steps):
+        agent.destroy(FORWARD)
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(DOWN)
+        agent.collect_all()
+        agent.move(FORWARD, 1)
+player.on_chat("mine", mine)
+
+def buildwall():
+    for i in range(5):
+        for i in range(3):
+            agent.place(FORWARD)
+            agent.move(UP, 1)
+        agent.move(DOWN, 3)
+        agent.move(RIGHT, 1)
+player.on_chat("wall", buildwall)
